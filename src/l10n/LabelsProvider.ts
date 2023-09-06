6c0f9b31c6a27593cb21df3ad02bf313750a84ce
David <braginsky.david@yandex.ru>
2023-09-07 00:36:53 +0300

    added hover styles

--- src/l10n/LabelsProvider.ts
+++ src/l10n/LabelsProvider.ts
@@ -4,5 +4,7 @@ export const LabelsProvider = {
   NEW_TODO: "новая задачка...",
   TOTAL_NUMBER: "Общее количество",
   COMPLETED: "Завершено",
-  FROM: "из"
+  FROM: "из",
+  INPUT_MUST_NOT_BE_EMPTY: "поле не может быть пустым"
+
 };
